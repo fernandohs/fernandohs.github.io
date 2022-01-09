02bff46e71f2d94836456b20deba8f4ba3f194e5
Fernando Herrera <fernandohs@live.com.mx>
2022-01-08 18:03:52 -0700

    Updates

---
+++ precache-manifest.d1276612cb5444655c30391bae77f860.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "662af10227b430c7f0ce9ebd16036c9e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9244d07c169489e70198",
+    "url": "/static/css/2.d64471e2.chunk.css"
+  },
+  {
+    "revision": "08237835bf7af802b3ad",
+    "url": "/static/css/main.fc056117.chunk.css"
+  },
+  {
+    "revision": "9244d07c169489e70198",
+    "url": "/static/js/2.6268b205.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "/static/js/2.6268b205.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "08237835bf7af802b3ad",
+    "url": "/static/js/main.ddb7d69c.chunk.js"
+  },
+  {
+    "revision": "a2d41c56873c741b51b2",
+    "url": "/static/js/runtime-main.a5042f6b.js"
+  }
+]);
